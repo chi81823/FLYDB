84a9c93823d4bb8a8dacd0945069c1b5e5d1f1f7
Darren <chi0927270823@gmail.com>
2019-08-30 10:08:22 +0800

    init

--- MIGRATION/AP/V1.0__FLYDB_INSERT_TESTTABLE.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO account(user_id,username,password,email,created_on,last_login) VALUES('000001','test1','test1','test1@cathayholdings.com.tw',sysdate,sysdate);
-INSERT INTO account(user_id,username,password,email,created_on,last_login) VALUES('000002','test2','test2','test2@cathayholdings.com.tw',sysdate,sysdate);
-INSERT INTO account(user_id,username,password,email,created_on,last_login) VALUES('000003','test3','test3','test3@cathayholdings.com.tw',sysdate,sysdate);
-INSERT INTO account(user_id,username,password,email,created_on,last_login) VALUES('000004','test4','test4','test4@cathayholdings.com.tw',sysdate,sysdate);
-INSERT INTO account(user_id,username,password,email,created_on,last_login) VALUES('000005','test5','test5','test5@cathayholdings.com.tw',sysdate,sysdate);
